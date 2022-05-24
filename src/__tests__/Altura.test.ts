730042ab28667a14c97ca505d5e11e664eabe84d
thehighmountain <thehighmountain25@gmail.com>
2022-05-25 00:27:17 +0900

    update altura test

--- src/__tests__/Altura.test.ts
+++ src/__tests__/Altura.test.ts
@@ -6,18 +6,17 @@ const logger = new Logger().addHandler(new ConsoleHandler(LogLevel.DEBUG));
 
 const altura = new Altura('123456789', (arg: string) => logger.info(arg));
 
-const user: AlturaUser = {
-  address: '0x79c670b2dc9aff7d0c1a4c8a9a3a4b9a3ce0c071',
-  name: '📊',
-  bio: '🎨🧑‍🎨🖼',
-  profilePicUrl:
-    'https://altura-marketplace-1.s3.us-east-2.amazonaws.com/0x79c670b2dc9aff7d0c1a4c8a9a3a4b9a3ce0c071_98ba883f-0c1e-41ba-b8b6-a30515684dd0.png',
-  socialLink: '',
-};
-
 test('Altura get users', () => {
   return altura.getUsers(1, 1, 'name', 'desc').then((data) => {
-    expect(data.users[0]).toStrictEqual(user);
-    expect(data.count).toBe(41893);
+    expect(data.users[0]).toEqual(
+      expect.objectContaining({
+        address: expect.any(String),
+        name: expect.any(String),
+        bio: expect.any(String),
+        profilePicUrl: expect.any(String),
+        socialLink: expect.any(String),
+      }),
+    );
+    expect(typeof data.count).toBe('number');
   });
 });
